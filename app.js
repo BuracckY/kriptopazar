2437589285ea9b6a84545a03161dfa22de4c9fa4
BuracckY <bburakyararr@hotmail.com>
2025-10-20 14:04:16 +0300

    Change admin route from '/admin' to '/s-panel-a4x9'

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 });
 
 // --- Rotalar ---
-app.use('/admin', adminRoutes);
+app.use('/s-panel-a4x9', adminRoutes);
 app.use('/', mainRoutes);
 
 // --- Telegram Bot ---
